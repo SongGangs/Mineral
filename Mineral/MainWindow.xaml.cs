29d06a622c268d1386e81f8cd467b0aa755800cc
SongGangs <sg951205@vip.qq.com>
2017-04-10 11:05:32 +0800

    sg

--- Mineral/MainWindow.xaml.cs
+++ Mineral/MainWindow.xaml.cs
@@ -88,7 +88,6 @@ namespace Mineral
         /// <param name="ds"></param>
         private void InitDataGridByDataSet(DataSet ds)
         {
-            /*
             displays = new ObservableCollection<CDisplay>();
             for (int i = 0; i < ds.Tables.Count; i++)
             {
@@ -100,7 +99,7 @@ namespace Mineral
                     });
                 }
             }
-            this.DataGrid.ItemsSource = displays;*/
+            this.DataGrid.ItemsSource = displays;
         }
         /// <summary>
         /// 通过实体将文本框初始化
