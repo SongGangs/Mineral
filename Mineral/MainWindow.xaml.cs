dc139685aac8343e6112088016612bc4a2189611
SongGangs <sg951205@vip.qqq.com>
2017-04-10 09:33:25 +0800

    我是测试机

--- Mineral/MainWindow.xaml.cs
+++ Mineral/MainWindow.xaml.cs
@@ -86,7 +86,6 @@ namespace Mineral
             dlg.ShowDialog();
             if (!string.IsNullOrEmpty(dlg.FileName))
             {
-                Console.WriteLine("....");
                 MessageBox.Show(ExcelHelper.ExportDataToAccess(dlg.FileName, OrginMineralDs));
             }
             
